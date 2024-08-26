dec1197d5b55d1a93c2270a7b7d74eb3ae82ac07
matthewkim1005 <matt_hew_kim@yahoo.com>
2024-08-26 10:55:18 -0700

    Added reminder full CRUD.

--- controllers/reminders.js
+++ controllers/reminders.js
@@ -5,4 +5,61 @@ const router = express.Router();
 
 router.use(verifyToken);
 
+router.post('/', async (req, res) => {
+    try {
+        const reminder = await Reminder.create(req.body);
+        res.status(201).json(reminder);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const reminders = await Reminder.find({})
+        res.status(200).json(reminders);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.get('/:reminderId', async (req, res) => {
+    try {
+        const reminder = await Reminder.findById(req.params.reminderId);
+        res.status(200).json(reminder);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.put('/:reminderId', async (req, res) => {
+    try {
+        const reminder = await Reminder.findById(req.params.reminderId);
+
+        // Update Reminder:
+        const updatedReminder = await Reminder.findByIdAndUpdate(
+            req.params.reminderId,
+            req.body,
+            { new: true }
+        );
+
+        // Issue JSON response:
+        res.status(200).json(updatedReminder);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.delete('/:reminderId', async (req, res) => {
+    try {
+        const reminder = await Reminder.findById(req.params.reminderId);
+
+        const deletedReminder = await Reminder.findByIdAndDelete(req.params.reminderId);
+        res.status(200).json(deletedReminder);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 module.exports = router;
